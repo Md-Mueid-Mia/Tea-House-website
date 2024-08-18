3ed380228b44c32bd9a0ccdaf5e48410c3881576
Md Mueid MiaMd-Mueid-Mia <mdmueidmia@gmail.com>
2024-08-18 12:57:06 +0600

    this website all section complate also responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,11 @@ module.exports = {
     theme: {
       extend: {},
     },
-    plugins: [],
+    // tailwind.config.js
+plugins: [
+  require("@tailwindcss/forms")({
+    strategy: 'base', // only generate global styles
+    strategy: 'class', // only generate classes
+  }),
+],
   }
